f4775691a7d6cd0805f1c7bcbf7d6c779dfce262
Thomas Jakobi <thomas.jakobi@partout.info>
2020-11-26 08:22:42 +0100

    Fix an uninstall issue - the service class could not be available anymore then

--- core/components/switchtemplate/elements/plugins/switchtemplate.plugin.php
+++ core/components/switchtemplate/elements/plugins/switchtemplate.plugin.php
@@ -17,12 +17,14 @@ $switchtemplate = $modx->getService('switchtemplate', 'SwitchTemplate', $corePat
     'core_path' => $corePath
 ));
 
-$modx->loadClass('SwitchTemplatePlugin', $switchtemplate->getOption('modelPath') . 'switchtemplate/events/', true, true);
-$modx->loadClass($className, $switchtemplate->getOption('modelPath') . 'switchtemplate/events/', true, true);
-if (class_exists($className)) {
-    /** @var SwitchTemplatePlugin $handler */
-    $handler = new $className($modx, $scriptProperties);
-    $handler->run();
+if ($switchtemplate) {
+    $modx->loadClass('SwitchTemplatePlugin', $switchtemplate->getOption('modelPath') . 'switchtemplate/events/', true, true);
+    $modx->loadClass($className, $switchtemplate->getOption('modelPath') . 'switchtemplate/events/', true, true);
+    if (class_exists($className)) {
+        /** @var SwitchTemplatePlugin $handler */
+        $handler = new $className($modx, $scriptProperties);
+        $handler->run();
+    }
 }
 
 return;
