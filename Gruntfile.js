ec4c2807eed1562c13ac8492a9ed31ddf219c71f
Thomas Jakobi <thomas.jakobi@partout.info>
2021-12-29 11:44:30 +0100

    Normalize code

--- Gruntfile.js
+++ Gruntfile.js
@@ -145,8 +145,8 @@ module.exports = function (grunt) {
                 }],
                 options: {
                     replacements: [{
-                        pattern: /© \d{4}(-\d{4})? by/g,
-                        replacement: '© ' + (new Date().getFullYear() > 2014 ? '2014-' : '') + new Date().getFullYear() + ' by'
+                        pattern: /&copy; \d{4}(-\d{4})? by/g,
+                        replacement: '&copy; ' + (new Date().getFullYear() > 2014 ? '2014-' : '') + new Date().getFullYear() + ' by'
                     }]
                 }
             },
--- source/js/mgr/widgets/home.panel.js
+++ source/js/mgr/widgets/home.panel.js
@@ -25,7 +25,7 @@ SwitchTemplate.panel.Home = function (config) {
                 afterrender: function (component) {
                     component.getEl().select('img').on('click', function () {
                         var msg = '<span style="display: inline-block; text-align: center"><img src="' + SwitchTemplate.config.assetsUrl + 'img/mgr/treehill-studio.png" srcset="' + SwitchTemplate.config.assetsUrl + 'img/mgr/treehill-studio@2x.png 2x" alt="Treehill Studio"><br>' +
-                            '© 2014-2020 by <a href="https://treehillstudio.com" target="_blank">treehillstudio.com</a></span>';
+                            '&copy; 2014-2020 by <a href="https://treehillstudio.com" target="_blank">treehillstudio.com</a></span>';
                         Ext.Msg.show({
                             title: _('switchtemplate') + ' ' + SwitchTemplate.config.version,
                             msg: msg,
