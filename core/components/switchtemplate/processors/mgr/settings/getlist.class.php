3e358b6e445dfc1989cd8f93065c75b2afa0e525
Thomas Jakobi <thomas.jakobi@partout.info>
2025-02-11 18:36:43 +0100

    - Check the namespaced settings permission for the listing of the system settings too

--- core/components/switchtemplate/processors/mgr/settings/getlist.class.php
+++ core/components/switchtemplate/processors/mgr/settings/getlist.class.php
@@ -13,11 +13,23 @@ if (file_exists(MODX_PROCESSORS_PATH . 'system/settings/getlist.class.php')) {
     class_alias(\MODX\Revolution\Processors\System\Settings\GetList::class, \modSystemSettingsGetListProcessor::class);
 }
 
+/**
+ * Class SwitchTemplateSystemSettingsGetlistProcessor
+ */
 class SwitchTemplateSystemSettingsGetlistProcessor extends modSystemSettingsGetListProcessor
 {
     public $languageTopics = ['setting', 'namespace', 'switchtemplate:setting'];
     public $objectType = 'setting';
     public $primaryKeyField = 'key';
+
+    /**
+     * {@inheritDoc}
+     * @return bool
+     */
+    public function checkPermissions()
+    {
+        return !empty($this->permission) ? $this->modx->hasPermission($this->permission) || $this->modx->hasPermission('switchtemplate_' . $this->permission) : true;
+    }
 
     /**
      * {@inheritDoc}
